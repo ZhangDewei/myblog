ec6583ce3d8d698b13f07fd5e943c639efe0b031
dewei <zhangdewei@guihua.com>
2015-09-29 15:22:21 +0800

    后台权限认证

--- controllers/dashboard/index.go
+++ controllers/dashboard/index.go
@@ -2,12 +2,36 @@ package dashboard
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"myblog/lib"
+	"myblog/models"
 )
 
 type IndexDBController struct {
 	beego.Controller
 }
 
+func ValidMaster(ctx *context.Context) ([]*models.User, bool) {
+	user, err := lib.GetUserByCookie(ctx)
+	if err == false {
+		return user, false
+	}
+	if user[0].Super == 0 {
+		return user, false
+	}
+	return user, true
+}
+
 func (this *IndexDBController) Get() {
+	user, err := ValidMaster(this.Ctx)
+	if err == false {
+		this.Redirect("/", 301)
+		return
+	}
+	this.Data["username"] = user[0].Name
 	this.TplNames = "dashboard/index.html"
+	this.Layout = "dashboard/layout.html"
+	this.LayoutSections = make(map[string]string)
+	this.LayoutSections["HtmlHead"] = "dashboard/index_head.html"
+	this.LayoutSections["Script"] = "dashboard/index_script.html"
 }
